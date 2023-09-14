145cd4f50789ea89aecb4001f52aebb427d11e86
ta-design2021 <takenori.aino@gmail.com>
2023-09-14 15:27:32 +0900

    新規登録できる場合のテストコードを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { Faker::Name.last_name }
+    first_name            { Faker::Name.first_name }
+    last_name_kana        { Faker::Name.last_name }
+    first_name_kana       { Faker::Name.first_name }
+    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) } # ランダムな生年月日を設定
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,37 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    # it 'nicknameが空では登録できない' do
+    #   @user.nickname = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    # end
+    # it 'emailが空では登録できない' do
+    #   @user.email = ''
+    #   @user.valid?
+    #   expect(@user.errors.full_messages).to include "Email can't be blank"
+    # end
+
+    it 'nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birth_dateが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
     it 'nicknameが空では登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合は登録できない' do
+    end
+    it 'emailは@を含まないと登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
+    end
+    it 'passwordが129文字以上では登録できない' do
     end
   end
 end
