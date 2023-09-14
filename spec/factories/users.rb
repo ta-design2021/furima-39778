d2522c6fee522602297290f474b148b9eef16860
ta-design2021 <takenori.aino@gmail.com>
2023-09-14 22:34:21 +0900

    Rubocopを実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,10 +7,6 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    # last_name             { Faker::Name.last_name }
-    # first_name            { Faker::Name.first_name }
-    # last_name_kana        { Faker::Name.last_name }
-    # first_name_kana       { Faker::Name.first_name }
     last_name             { person.last.kanji }
     first_name            { person.first.kanji }
     last_name_kana        { person.last.katakana }
