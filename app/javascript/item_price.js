006042ca8c7e3fee887067ab38708492a71b941d
ta-design2021 <takenori.aino@gmail.com>
2023-09-17 18:04:40 +0900

    販売手数料や販売利益の計算をJSで実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,12 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OKOK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    // 入力した金額に販売手数料（10%）を掛ける計算をする処理
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    // 販売価格から販売手数料を引く処理
+    profitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+  })
 });
