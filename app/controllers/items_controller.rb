3837de8ad1adbc3405f117c1d1dbd4bf1ff59733
ta-design2021 <takenori.aino@gmail.com>
2023-09-18 17:54:25 +0900

    詳細ページへの遷移、表示の実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
   before_action :authenticate_user!, except: [:index, :show]
+  # 重複処理のまとめ
+  before_action :set_item, only: [ :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,10 +21,17 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
