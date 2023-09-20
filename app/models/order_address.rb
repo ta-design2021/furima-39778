3103b7aded34069d0b17d2fc2922bb359edac288
ta-design2021 <takenori.aino@gmail.com>
2023-09-20 17:39:56 +0900

    ordersテーブル、addressesテーブル作成、購入機能に必要なルーティングを設定

---
+++ app/models/order_address.rb
@@ -0,0 +1,26 @@
+class OrderAddress
+  include ActiveModel::Model
+  # ordesテーブルとaddressesテーブルに保存したいカラム名をすべて指定
+  attr_accessor :user_id, :item_id, :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number
+
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id # order_addressクラスにはアソシエーションを定義することはできないため、belongs_toによるバリデーションを行うことができないのでorder_addressクラスでuser_idに対してバリデーションを新たに設定。
+    validates :item_id
+    # addressesモデルのバリデーション
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :item_prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid. Input only number' }
+  end
+
+  # フォームから送られてきた情報をテーブルに保存する処理
+  def save
+    # 購入情報を保存し、変数orderに代入する
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 住所を保存する
+    # order_idには、変数orderのidと指定する
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
+  end
+end
