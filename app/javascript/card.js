c3950a75338fc282ea84c90046182d0ba2091bbc
ta-design2021 <takenori.aino@gmail.com>
2023-09-23 19:55:48 +0900

    トークン生成

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,23 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_338810afeb8c27dcf1e8118c')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    console.log("フォーム送信時にイベント発火")
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
     e.preventDefault();
   });
 };
